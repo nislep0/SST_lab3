da42ba1f66d6bbac1517b00c9311e207e8a6284e
nislep0 <ruden1603@gmail.com>
2025-11-27 18:40:26 +0200

    add tests for logic and io layers

---
+++ tests/test_logic.py
@@ -0,0 +1,55 @@
+from re import A
+from tetris.logic import (
+    parse_field_from_lines,
+    field_to_lines,
+    drop_piece,
+    InvalidInputError,
+    )
+
+def test_parse_and_serilise_simple():
+    lines = [
+        "3 3\n",
+        "..p\n",
+        ".#.\n",
+        "...\n",                
+        ]
+    field = parse_field_from_lines(lines)
+    outlines = field_to_lines(field)
+    assert outlines == ["..p", ".#.", "..."]
+
+def test_drop_piece_ex1():
+    lines = [
+        "7 8\n",
+        "..p.....\n",
+        ".ppp....\n",
+        "..p.....\n",
+        "........\n",
+        "...#....\n",
+        "...#...#\n",
+        "#..#####\n",
+    ] 
+    field = parse_field_from_lines(lines)
+    finalfield = drop_piece(field)
+    out = field_to_lines(finalfield)
+    assert out == [
+        "........",
+        "........",
+        "..p.....",
+        ".ppp....",
+        "..p#....",
+        "...#...#",
+        "#..#####",
+        ]
+
+def test_invalid_no_piece():
+    lines = [
+        "2 2\n",
+        "##\n",
+        "..\n",
+    ]
+    try:
+        parse_field_from_lines(lines)
+        assert False, "Expected InvalidInputError"
+    except InvalidInputError:
+        pass
+
